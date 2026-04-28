a490dec4899b68a1328c3ad124080a8f79cc44f6
Abhilash Kumar <abhilashh303@gmail.com>
2026-04-28 21:21:40 +0530

    final fixed OTP automation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
--- tests/test_credit_card.py
+++ tests/test_credit_card.py
@@ -1,4 +1,5 @@
-from pages.credit_card_page import CreditCardPage   # 👈 ye add karo
+from pages.credit_card_page import CreditCardPage
+
 
 def test_credit_card_flow(driver):
     page = CreditCardPage(driver)
@@ -6,11 +7,9 @@ def test_credit_card_flow(driver):
     page.open()
     page.fill_form()
 
-    page.wait_for_manual_otp()
+    # 🔥 Fixed OTP
+    page.enter_fixed_otp("1234")
 
     page.click_verify()
-    page.click_continue()
 
-    msg = page.verify_success()
-
-    assert "Thank" in msg
+    assert page.is_success()
